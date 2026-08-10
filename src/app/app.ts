a1b9d4604dfd562ad039316df40ecf446418684c
Javi <Javi>
2026-08-10 17:43:36 +0200

    Ajuste de layout, hover en tarjetas y footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Courses } from './courses/courses';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Courses],
+  imports: [RouterOutlet, Courses, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
