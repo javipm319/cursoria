61e3754dbc049025dae4e535d0eeca3d46a50720
Javi Pérez <Javi>
2026-08-26 09:59:39 +0200

    Header y ajuste de padding lateral

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Courses } from './courses/courses';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Courses, Footer],
+  imports: [RouterOutlet, Courses, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
